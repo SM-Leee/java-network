ee69ae61c80da33c7ba8a61d6c9bb8ff42a9f8ba
BIT <BIT@DESKTOP-E23PFRN>
2019-01-08 16:06:23 +0900

    190108 06:05 NSLOOKUP 구현하기 ( InterAddress 클래스 이용)

--- src/util/NSLookup.java
+++ src/util/NSLookup.java
@@ -2,21 +2,28 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 //과제
 public class NSLookup {
 
 	public static void main(String[] args) {
-		String line = "www.naver.com";
-		
-		try {
-			InetAddress[] inetAddress = InetAddress.getAllByName(line);
-			for(InetAddress inetAddres : inetAddress) {
-				System.out.println(inetAddres);
+		Scanner sc = new Scanner(System.in);
+		String line = null;
+		while(true) {
+			System.out.print("> ");
+			line = sc.nextLine();
+			if("exit".equals(line)) {
+				break;
 			}
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-		
+			try {
+				InetAddress[] inetAddress = InetAddress.getAllByName(line);
+				for(InetAddress inetAddres : inetAddress) {
+					System.out.println(inetAddres);
+				}
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+		}		
 		
 
 	}
